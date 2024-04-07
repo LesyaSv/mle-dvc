93a25f32fef57754310345865615922edfc020a3
Lesya_Sv <mle-user@epd4jp7amuov6kgqe6so.auto.internal>
2024-04-07 08:13:41 +0000

    обновление DVC-пайплайна

--- scripts/fit.py
+++ scripts/fit.py
@@ -20,31 +20,32 @@ def fit_model():
     data = pd.read_csv('data/initial_data.csv')
     # реализуйте основную логику шага с использованием гиперпараметров
     cat_features = data.select_dtypes(include='object')
-    potential_binary_features = cat_features.nunique() == 2
-    
-    binary_cat_features = cat_features[potential_binary_features[potential_binary_features].index]
-    other_cat_features = cat_features[potential_binary_features[~potential_binary_features].index]
     num_features = data.select_dtypes(['float'])
-    
-    preprocessor = ColumnTransformer([
-        ('binary', OneHotEncoder(drop= params['one_hot_drop']), binary_cat_features.columns.tolist()),
-        ('cat', CatBoostEncoder(return_df=False), other_cat_features.columns.tolist()),
-        ('num', StandardScaler(), num_features.columns.tolist())
-    ],
+
+    preprocessor = ColumnTransformer(
+        [
+            ('cat', OneHotEncoder(drop=params['one_hot_drop']), cat_features.columns.tolist()),
+            ('num', StandardScaler(), num_features.columns.tolist())
+        ],
         remainder='drop',
         verbose_feature_names_out=False
     )
-    
-    model = CatBoostClassifier(auto_class_weights=params['auto_class_weights'])
-    
+
+    model = LogisticRegression(C=params['log_c'], penalty=params['log_penalty'])
+
+    pipeline = Pipeline(
+        [
+            ('preprocessor', preprocessor),
+            ('model', model)
+        ]
+    )
+    pipeline.fit(data, data[params['target_col']]) 
     pipeline = Pipeline(
         [
             ('preprocessor', preprocessor),
             ('model', model)
         ])
-    pipeline.fit(data, data[params['target_col']]) 
-
-	# сохраните обученную модель в models/fitted_model.pkl
+    # сохраните обученную модель в models/fitted_model.pkl
     os.makedirs('models', exist_ok=True)
     with open('models/fitted_model.pkl', 'wb') as fd:
         joblib.dump(pipeline, fd) 
